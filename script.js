45e137e3a2040dbf6b59b60dd68cf945b11a7abd
shaurya101 <71705373+shaurya101@users.noreply.github.com>
2021-11-20 13:29:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+// Displaying slider number
+// Syncing value of slider and arrow buttons 
+
+const slider = document.querySelector('#slider');
+const minus1 = document.querySelector('#minus1');
+const plus1 = document.querySelector('#plus1');
+const inputDisplay = document.querySelector('#inputDisplay');
+
+
+slider.addEventListener('input', function(){
+     inputDisplay.textContent = slider.value;
+});
+minus1.addEventListener('click', function(){
+    slider.value--;
+    inputDisplay.textContent = slider.value;
+});
+plus1.addEventListener('click', function(){
+    slider.value++;
+    inputDisplay.textContent = slider.value;
+});
 
